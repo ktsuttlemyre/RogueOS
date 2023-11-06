4579eea554aacfa6bdd58771e1d0da809e585c90
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2023-11-06 13:30:43 -0500

    Update init.sh

--- cloud-init/init.sh
+++ cloud-init/init.sh
@@ -33,20 +33,22 @@ apt update & sudo apt install -y xinit i3 dmenu suckless-tools
 #Add i3 to our xinit.rc file so startx will run i3
 echo "exec i3" > .xinit.rc
 
-#Start xserver
-startx
-
 #run raspi-config-> boot, select auto login desktop
 
 ##### Install kodi and chrome #####
-apt install -y kodi chromium-browser seahorseexit
+apt install -y kodi chromium-browser seahorse
 
 
 ##### Download Advanced Launcher by typing
-wget https://github.com/SpiralCut/plugin.program.advanced.launcher/archive/master.zip
-Type nano chromium.sh and enter the following file contents:
-#! /bin/bash
+#wget https://github.com/SpiralCut/plugin.program.advanced.launcher/archive/master.zip
+#Type nano chromium.sh and enter the following file contents:
+##! /bin/bash
 #chromium-browser --user-agent="Mozilla/5.0 (X11; CrOS armv7l 10895.56.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.102 Safari/537.36" --window-size=1920,1080 $1
 
-Return to the pi and open Kodi. Install Advanced Launcher.
-Navigate to Add-ons | Add-On Browser (the open box at the top next to the Settings gear)
+#Return to the pi and open Kodi. Install Advanced Launcher.
+#Navigate to Add-ons | Add-On Browser (the open box at the top next to the Settings gear)
+
+
+
+#Start xserver
+startx
