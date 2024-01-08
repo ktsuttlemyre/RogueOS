10404ad64a7d4ffbcf2d6cf0865c2539b7e80a66
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-01-08 11:51:14 -0500

    Update init.sh

--- hosts/KQSFL/init.sh
+++ hosts/KQSFL/init.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-echo "TMP init file"
+echo "Installing cab1kqsfl config"
+
+cd 
+git clone https://github.com/tiagostutz/docker-traefik-letsencrypt.git
+cd ./docker-traefik-letsencrypt/tls
+docker compose build
+
+git clone https://github.com/ktsuttlemyre/RogueOS
+cd ./RogueOS/instances/kqsfl_cab1
+#all service_containers found at RogueOS/service_containers
+
+../utils/runservices.sh
+
