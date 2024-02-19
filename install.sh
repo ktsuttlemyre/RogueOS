ed07f5f7766c110a70cf47315eeb5b308100f344
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-02-19 14:11:53 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -130,7 +130,7 @@ fi
 if [ -f /etc/os-release ]; then
   source /etc/os-release
 else
-  source $rogue_wdir/scripts/os-release.sh
+  source /dev/stdin <<< "$(curl -s https://raw.githubusercontent.com/ktsuttlemyre/RogueOS/master/scripts/os-release.sh)"
 fi
 ID="${ID:-$OS}"
 
