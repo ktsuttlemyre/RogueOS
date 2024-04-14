3064646b859847658fb166348afce5ba0a71ef0e
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-04-14 18:35:02 -0400

    Create scan_network_for_hosts.sh

---
+++ scripts/scan_network_for_hosts.sh
@@ -0,0 +1,17 @@
+#/!bin/bash
+# https://raspberrypi.stackexchange.com/questions/13936/find-raspberry-pi-address-on-local-network
+
+#search arp table (local cache of hosts) (fast)
+arp -na
+
+#actively iterate all connection devices to find hosts (slow)
+devices=$(nmcli device status |  tail -n +2 | cut -d" " -f1) 
+IFS=$'\n' ;for device in $devices ; do 
+  network=$(nmcli dev show $device | grep "GATEWAY" |  tr -s ' ' | cut -d" " -f2)
+  IFS=$'\n' ;for mask in $network ; do 
+    #todo fix ipv6 cider notation
+    nmap -sP $mask/24
+  done
+done
+
+echo "done"
