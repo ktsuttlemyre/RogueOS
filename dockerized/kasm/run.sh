2fbc535eaa4a6a1e27254f363994cded3dad4b31
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-13 18:34:18 -0400

    Update run.sh

--- dockerized/kasm/run.sh
+++ dockerized/kasm/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker run --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password rogueos/rogueos:latest
+docker run --privileged --rm -it --shm-size=512m -p 6901:6901 -e VNC_PW=password rogueos/rogueos:latest
