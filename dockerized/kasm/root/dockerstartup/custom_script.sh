379026eb85ff4063203e5d780613a9363326e532
ktsuttlemyre <ktsuttlemyre@users.noreply.github.com>
2024-05-15 20:41:09 -0400

    Update custom_script.sh

--- dockerized/kasm/root/dockerstartup/custom_script.sh
+++ dockerized/kasm/root/dockerstartup/custom_script.sh
@@ -4,9 +4,10 @@ exit 0
 #TODO run RSYNC to rogueos_data
 
 #SYMLINK only things that will sync
-files=(/rogueos_data/$host_name/$user_name/*)
-ARRAY+=('/rogueos_data/$host_name/$user_name/.config/obs-studio')
-ARRAY+=('/rogueos_data/$host_ name/$user_name/.config/rclone')
+SYMLINK_FILES='/rogueos_data/$host_name/$user_name/*'
+files=($SYMLINK_FILES)
+files+=('/rogueos_data/$host_name/$user_name/.config/obs-studio')
+files+=('/rogueos_data/$host_ name/$user_name/.config/rclone')
 
 for file in "${files[@]}"; do
   if [[ $file = .* ]]; then
